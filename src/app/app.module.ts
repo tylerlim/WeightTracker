dbef86b91c556945ccc1112647ae9584aad1194c
tylerlim <tylerjsc@gmail.com>
2018-06-26 01:37:14 +0800

    add angular route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {  RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BmiCalculaterComponent } from './bmi-calculater/bmi-calculater.component';
+
+const routes: Routes = [
+  // basic routes
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'bmiCalculater', component: BmiCalculaterComponent },
+  { path: 'profile', component: ProfileComponent }
+];
 
 @NgModule({
   declarations: [
@@ -14,9 +22,11 @@ import { BmiCalculaterComponent } from './bmi-calculater/bmi-calculater.componen
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
